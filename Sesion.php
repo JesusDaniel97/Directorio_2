b1deadbfefa8f475c55f30e7277ed11598c14b57
JesusDaniel97 <102211446+JesusDaniel97@users.noreply.github.com>
2023-11-21 14:30:10 -0600

    Update Sesion.php

--- Sesion.php
+++ Sesion.php
@@ -1,3 +1,11 @@
+<?php
+   session_start();
+   if(!isset($_SESSION["usuario"])){
+      header("Location:index.html");
+   }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,13 +18,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    
+
 </head>
 <body>
   <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
     <div class="bg-dark p-4">
-      <h5 class="text-body-emphasis h4">USUARIO</h5>
-      <span class="text-body-secondary"><a href="">Cerra Sesion</a></span>
+      <?php echo "<li><h3 style='color:white;'>".$_SESSION["usuario"]."</i></h3>";?>
+      <span class="text-body-secondary"><a href="Cerrar_Sesion.php">Cerrar Sesion</a></span>
     </div>
   </div>
   <nav class="navbar navbar-dark bg-dark">
@@ -30,24 +38,24 @@
       <center><h2>BUSQUEDA DE PERITOS </h2></center>
   </div> 
   <div id="formulario">
-    <form class="row g-3 needs-validation" novalidate>
+    <form class="row g-3 needs-validation" novalidate method="get">
       <div class="col-md-4">
         <label for="validationCustom01" class="form-label">Nombre</label>
-        <input type="text" class="form-control" id="validationCustom01" required><br>
+        <input type="text" class="form-control" id="validationCustom01" name="nombre" required><br>
       </div>
       <div class="col-md-4">
         <label for="validationCustom02" class="form-label">Apellido</label>
-        <input type="text" class="form-control" id="validationCustom02" required><br>
+        <input type="text" class="form-control" id="validationCustom02" name="apellidos"  required><br>
       </div>
       <div class="col-md-4">
         <label for="validationCustomUsername" class="form-label">Registro</label>
         <div class="input-group has-validation">
-          <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required><br>
+          <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" name="registro" required><br>
         </div>
       </div>
       <div class="col-md-3">
         <label for="validationCustom04" class="form-label">Busqueda por estado</label>
-        <select class="form-select" id="validationCustom04" required>
+        <select class="form-select" id="validationCustom04" name="estado"  required>
           <option selected disabled value="">Estado...</option>
           <option value="AGUACALIENTES">AGUACALIENTES</option>
           <option value="BAJA CALIFORNIA">BAJA CALIFORNIA</option>
@@ -88,13 +96,15 @@
       </div>
         <center>
           <div class="col-12">
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa fa-search"></i>BUSCAR</button>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" name="buscar"><i class="fa fa-search"></i>BUSCAR</button>
+            <button type="submit" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" name="buscar"><i class="fa fa-search"></i>BUSCAR</button>
            </div>
         </center>
     </form>
+
   </div>
   <div id="Peritos">
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
@@ -102,7 +112,52 @@
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <form>
+              <?php
+                    require("conexion.php");
+                    $nombre = "";
+                    $apellido = "";
+                    $estado = "";
+                    $registro = "";
+                    if(!empty($_GET["nombre"])){
+                       $nombre = $_GET["nombre"];
+                    }
+                    if(!empty($_GET["apellidos"])){
+                       $apellido = $_GET["apellidos"];
+                    }
+                 
+                    if(!empty($_GET["estado"])){
+                       $estado = $_GET["estado"];
+                    }
+                 
+                    if(!empty($_GET["estado"])){
+                       $estado = $_GET["estado"];
+                    }
+                 
+                    if(!empty($_GET["registro"])){
+                       $registro = $_GET["registro"];
+                    }
+                    
+                    
+                    if(isset($_GET["buscar"])){
+                      $conexion = new mysqli($servername, $username, $password,$database);
+                      $consulta = "SELECT * FROM contactos WHERE Nombre = '$nombre' OR Apellidos = '$apellido' OR Estado_provincia='$estado' OR Registros='$registro'";
+                      
+                      $resultado = $conexion->query($consulta);
+                      if($resultado -> num_rows > 0){
+                        while($fila = $resultado->fetch_assoc()){
+                           echo "<p>".$fila["Nombre"]."</p>";
+                           echo "<p>".$fila["Apellidos"]."</p>";
+                           echo "<p>".$fila["Correoelectronico"]."</p>"; 
+                           echo "<p>".$fila["Telefonoparticular"]."</p>";  
+                           echo "<p>".$fila["Telefonomovil"]."</p>";  
+                           echo "<p>".$fila["Registros"]."</p>";  
+                           echo "<p>".$fila["Estado_provincia"]."</p>";
+                           echo "<p>".$fila["Notas"]."</p>";
+                           echo "<p>".$fila["Sin_viaticos"]."</p>";       
+                        }
+                      }
+                    }
+              ?>
               <p>pruebaaaaaaa</p>
               <p>nombre</p>
               <p>apellido</p>
@@ -110,21 +165,19 @@
               <p>telefono</p>
               <p>registro</p>
               <div class="mb-3">
-                <input type="text" name="nombre">
                 <label for="message-text" class="col-form-label">Message:</label>
                 <textarea class="form-control" id="message-text"></textarea>
               </div>
-            </form>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Send message</button>
+            <button type="submit" class="btn btn-primary">Send message</button>
           </div>
         </div>
       </div>
     </div>
-   </div>
-  <script src="sesion.js"></script>
+  </div>
+
   
 </body>
 </html>
