9114cf6ad9bca7ba140275f8e28ff063c47a3969
JesusDaniel97 <102211446+JesusDaniel97@users.noreply.github.com>
2023-11-29 13:43:03 -0600

    Update Eliminar_perito.php

--- Eliminar_perito.php
+++ Eliminar_perito.php
@@ -8,25 +8,36 @@
 </head>
 <body>
     
+
     <div class="container mt-5">
     <div class="row">
     <div class="col-sm-6 offset-sm-3">
     <div class="alert alert-danger text-center">
-    <p>¿Desea confirmar la eliminacion del Perito </p>
+    <p>¿Desea confirmar la eliminacion del Perito <?php echo $_GET["nombre"]?></p>
     </div>
 
     <div class="row">
         <div class="col-sm-6">
-            <form action="Acciones.php" method="POST">
+            <form action="" method="POST">
                 <input type="hidden" name="accion" value="eliminar_registro">
                 <input type="hidden" name="nombre" value="<?php echo $_GET['nombre']; ?>">
-                <input type="submit" name="" value="Eliminar" class= " btn btn-danger">
+                <input type="submit" name="eliminar" value="Eliminar" class= " btn btn-danger">
                 <a href="Administrador.php" class="btn btn-success">Cancelar</a>
+           </form>    
                                
         </div>
     </div>
-    
-
+            <?php
+             if(isset($_POST["eliminar"])){
+                   require("conexion.php");
+                   $nombre = $_GET['nombre'];
+                   $conexion = mysqli_connect("localhost","root","","directorio");
+                   $consulta = "DELETE FROM contactos WHERE Nombre='$nombre'" ;
+                   mysqli_query($conexion,$consulta); 
+                   
+               } 
+            ?>
+          
 
 </body>
 </html>
