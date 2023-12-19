ef57de759a1c5ebd090edb28c119ebafa6523c6e
JesusDaniel97 <102211446+JesusDaniel97@users.noreply.github.com>
2023-12-19 13:22:23 -0600

    Update Registro_peritos.php

--- Registro_peritos.php
+++ Registro_peritos.php
@@ -10,69 +10,49 @@
     $estado = "";
     $notas = "";
     $datos_adjuntos;
-    
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if(empty($_POST["nombre"])){
-                echo "el nombre es requerido";
-            }else{
-                $nombre = $_POST["nombre"];
-            }
-            if(empty($_POST["apellidos"])){
-                echo "el apellido es requerido ";
-            }else{
-                $apellido = $_POST["apellidos"];
-            }
-                
-            if(empty($_POST["correo"])){
-                echo "el correo es requerido";
-            }else{
-                $correo = $_POST["correo"];
-            }
+        if(!empty($_POST["nombre"])){
+            $nombre = $_POST["nombre"];
+        }
+       
+        if(!empty($_POST["apellidos"])){
+            $apellido = $_POST["apellidos"];
+        }
             
-            if(empty($_POST["telefonoparticular"])){
-                echo "el telefono particular es requerido ";
-            }else{
-                $telefono_particular = $_POST["telefonoparticular"];
-            }
+        if(!empty($_POST["correo"])){
+            $correo = $_POST["correo"];
+        }
+        
+        if(!empty($_POST["telefonoparticular"])){
+            $telefono_particular = $_POST["telefonoparticular"];
+        }
+                    
+        if(!empty($_POST["telefonomovil"])){
+            $telefono_movil = $_POST["telefonomovil"];
+        }
+            
+        
+        if(!empty($_POST["registro"])){
+            $registros = $_POST["registro"];
+        }
 
-            if(empty($_POST["telefonomovil"])){
-                echo "el telefono movil es requerido ";
-            }else{
-                $telefono_movil = $_POST["telefonomovil"];
-            }
-
-            if(empty($_POST["registro"])){
-                echo "el registro es requerido ";
-            }else{
-                $registros = $_POST["registro"];
-            }
-
-            if(empty($_POST["Residencia"])){
-                echo "la residencia es requerda ";
-            }else{
-                $residencia = $_POST["Residencia"];
-            }
-
-            if(empty($_POST["estado"])){
-                echo "el estado es requerido ";
-            }else{
-                $estado = $_POST["estado"];
-            }
-            if(empty($_POST["notas"])){
-                echo "la nota es requerida ";
-            }else{
-                $notas = $_POST["notas"];
-            }
-            if(empty($_POST["archivo"])){
-                echo "el archivo es requerido ";
-            }else{
-                $datos_adjuntos = $_POST["archivo"];
-                
-            }
-    }
-         
-     
+        if(!empty($_POST["Residencia"])){
+            $residencia = $_POST["Residencia"];
+        }
+        
+        if(!empty($_POST["estado"])){
+            $estado = $_POST["estado"];
+        }
+        if(!empty($_POST["notas"])){     
+            $notas = $_POST["notas"];
+        }        
+     }
+        copy($_FILES['archivo']['tmp_name'],$_FILES['archivo']['name']);
+        $archivo = $_FILES['archivo']['name'];
+        echo $_FILES['archivo']['name'];
+        echo "<img src='$archivo'>";
+      
     require('conexion.php');
     $conexion = new mysqli($servername, $username, $password,$database);
     if (!$conexion) {
@@ -80,10 +60,10 @@
     }
 
     $sql = "INSERT INTO  contactos (Nombre, Apellidos ,Correoelectronico, Telefonoparticular,Telefonomovil,Registros,Residencia,Ciudad, Estado_provincia,Notas,Datos_adjuntos)
-    VALUES ('$nombre', '$apellido', '$correo','$telefono_particular','$telefono_movil','$registros','$residencia','$ciudad','$estado','$notas','$datos_adjuntos');";
+    VALUES ('$nombre', '$apellido', '$correo','$telefono_particular','$telefono_movil','$registros','$residencia','$ciudad','$estado','$notas','$archivo');";
 
     if (mysqli_multi_query($conexion, $sql)) {
-        echo "<script src='prueba.js'></script>";
+       // header("Location:Administrador.php");
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
     }
