85c1510775ef9d6cf549936d213da54200c0dbfc
JesusDaniel97 <102211446+JesusDaniel97@users.noreply.github.com>
2023-12-22 12:46:18 -0600

    Update prueba.php

--- prueba.php
+++ prueba.php
@@ -13,6 +13,22 @@
 
 </head>
 <body>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="prueba.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+</head>
+<body>
   <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
     <div class="bg-dark p-4">
        
@@ -30,7 +46,7 @@
       <center><h2>BUSQUEDA DE PERITOS </h2></center>
   </div> 
   <div id="formulario" >
-    <form class="row g-3 needs-validation" novalidate method="POST" action="">
+    <form class="row g-3 needs-validation" novalidate method="POST" action="archivos.php">
       <div class="col-md-4">
         <label for="validationCustom01" class="form-label">Nombre</label>
         <input type="text" class="form-control" id="validationCustom01" name="nombre" required><br>
@@ -129,7 +145,6 @@
                                     $estado=$conexion->real_escape_string($_POST['estado']);
                                     
                                     $query = "SELECT * FROM contactos WHERE Nombre = '$nombre' OR Apellidos = '$apellido' OR Estado_provincia='$estado' OR Registros='$registro'";
-                                    
 
                                 }
                                 
@@ -146,8 +161,7 @@
                                         <td><?php echo $fila['Registros']; ?></td>
                                         <td><?php echo $fila['Estado_provincia']; ?></td>
                                         <td><?php echo $fila['Notas']; ?></td> 
-                                        <td><a class="btn btn-info" href="info.php?nombre=<?php echo $fila['Nombre']?>">info</a></td>
-                                        <td><a class="btn btn-dark" href="archivos.php?nombre=<?php echo $fila['Nombre']?>">archivos</a></td>  
+                                        <td><a class="btn btn-dark" href="archivos.php?nombre=<?php echo $fila['Nombre']?>">archivos</a></td>
                                                 
                                   <?php 
                                       
@@ -171,6 +185,9 @@
 
     
    </div>
+  
+</body>
+</html>
   
 </body>
 </html>
