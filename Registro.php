ac263f3acb337d7d80f76d287401c54f14078633
JesusDaniel97 <102211446+JesusDaniel97@users.noreply.github.com>
2024-01-04 13:38:19 -0600

    Update Registro.php

--- Registro.php
+++ Registro.php
@@ -30,11 +30,18 @@
          $contraseña = $_POST["contrasenia"];
       }
 
-      if(empty($_POST["permiso"])){
-         echo "el permiso es requerido ";
-      }else{
-         $permiso = $_POST["permiso"];
+      if(!empty($_POST["permiso"])){
+          $permiso = $_POST["permiso"];
       }
+      
+      if($permiso == "Lector"){
+         $rol = 1;
+      }else if($permiso == "Administrador"){
+         $rol = 2;
+      }else if($permiso == "Archivo"){
+         $rol = 3;
+      } 
+          
       
    }     
 
@@ -45,8 +52,8 @@
     die("Conexion fallida: " . mysqli_connect_error());
   }
 
-   $sql = "INSERT INTO registro (Usuario, Contrasenia ,Correo, permiso)
-   VALUES ('$usuario', '$contraseña', '$correo','$permiso');";
+   $sql = "INSERT INTO registro (Usuario, Contrasenia ,Correo, id_permiso)
+   VALUES ('$usuario', '$contraseña', '$correo','$rol');";
 
    if (mysqli_multi_query($conexion, $sql)) {
         header("Location:Registro.html");
