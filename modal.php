34117d291ed40bd8edc9e1fc9e784eabcaad8c4e
JesusDaniel97 <102211446+JesusDaniel97@users.noreply.github.com>
2024-01-05 13:44:12 -0600

    Update modal.php

--- modal.php
+++ modal.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
     <title>Document</title>
 </head>
 <body>
-<div class="modal fade" id="modal<?php echo $fila['Nombre']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="modal<?php echo $fila['ID']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -16,7 +17,16 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <?php echo $fila['Nombre'];?>
+        <?php echo $fila['Nombre'];?><br>
+        <?php echo $fila['Apellidos'];?><br>
+        <?php echo $fila['Telefonoparticular'];?><br>
+        <?php echo $fila['Telefonomovil'];?><br>
+        <?php echo $fila['Correoelectronico'];?><br>
+        <?php echo $fila["Residencia"];?><br>
+        <?php echo $fila["Registros"];?><br>
+        <?php echo $fila["Ciudad"];?><br>
+        <?php echo $fila["Estado_provincia"];?><br>
+        
         
       </div>
       <div class="modal-footer">
