d39d3f3716ab6aeb26d2d0693a98fcaad0b47217
JesusDaniel97 <102211446+JesusDaniel97@users.noreply.github.com>
2024-02-01 13:16:27 -0600

    Update añadir_cobertura.js

--- añadir_cobertura.js
+++ añadir_cobertura.js
@@ -1,9 +1,14 @@
 function añadir_cobertura(){
 
+    
+   
     var cobertura = document.createElement("select");
     var nombre = document.createElement("option");
-    let option1Texto = document.createTextNode("estados");
+    var opcion1 = document.createElement("option");
+    let option1Texto = document.createTextNode("ESTADOS");
+    let option3Texto = document.createTextNode("AGUASCALIENTES");
     nombre.appendChild(option1Texto);
+    opcion1.appendChild(option3Texto);
 
     var municipio = document.createElement("select");
     var nombre2 = document.createElement("option");
@@ -12,6 +17,8 @@ function añadir_cobertura(){
 
     cobertura.appendChild(nombre);
     municipio.appendChild(nombre2);
+    cobertura.appendChild(opcion1);
     document.body.appendChild(cobertura);
     document.body.appendChild(municipio);
+
 }
