c1f4eec9531dc1d4409879f900ebe68357f569ae
JesusDaniel97 <102211446+JesusDaniel97@users.noreply.github.com>
2024-02-02 10:15:56 -0600

    Update cobertura.php

--- cobertura.php
+++ cobertura.php
@@ -8,9 +8,23 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-    <button onclick="añadir_cobertura()">Añadir Cobertura</button>
+    <div>
+        <form action="" method="post">
+         <div class="input-field col s12 m12 l6">
+                <select id="municipios_cobertura" name="municipios_cobertura" class="form-select" aria-label="Default select example"></select>
+                <label for="municipios_cobertura">Municipio</label>
+         </div>
+
+            <select id="cobertura" name="cobertura" class="form-select" aria-label="Default select example"></select>
+            <label for="cobertura">Estado</label>
+        </form>
+    </div>
+   
+    <input type="submit" value="añadir cobertura">
+
+    <button onclick="añadir_cobertura()">Añadir mas coberturas</button>
     <script src="añadir_cobertura.js"></script> 
-     
+    <script src="cobertura.js"></script>
  
 </body>
 </html>
