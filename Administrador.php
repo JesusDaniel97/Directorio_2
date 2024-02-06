ff9be03675634073e8fb33117df7ddf0edbfd70f
JesusDaniel97 <102211446+JesusDaniel97@users.noreply.github.com>
2024-02-06 13:54:15 -0600

    Update Administrador.php

--- Administrador.php
+++ Administrador.php
@@ -1,4 +1,3 @@
-
 <?php
    session_start();
    if(!isset($_SESSION["usuario"])){
@@ -13,18 +12,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="Administrador.css">
+    <link rel="stylesheet" href="prueba.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    
 </head>
 <body>
-  <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
+
+<div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
     <div class="bg-dark p-4">
-      <h5 style="color:white;">BIENVENIDO ADMINISTRADOR/A <?php echo $_SESSION["usuario"]?></h5> 
+    <h5 style="color:white;">BIENVENIDO ADMINISTRADOR/A <?php echo $_SESSION["usuario"]?></h5> 
       <a href="Registro_usuarios.php"> Agregar Usuario</a><br>
       <span class="text-body-secondary"><a href="Cerrar_Sesion.php">Cerrar Sesion</a></span>
     </div>
@@ -37,94 +38,58 @@
     </div>      
   </nav> 
   <div>
-      <center><h2>PERITOS</h2></center>
+      <center><h2>BUSQUEDA DE PERITOS </h2></center>
   </div> 
   <div id="formulario" >
     <form class="row g-3 needs-validation" novalidate method="POST" action="">
       <div class="col-md-4">
         <label for="validationCustom01" class="form-label">Nombre</label>
-        <input type="search" class="form-control" id="validationCustom01" name="nombre" required><br>
+        <input type="search" class="form-control" id="validationCustom01" name="nombre" placeholder="Nombre" required><br>
       </div>
+
       <div class="col-md-4">
         <label for="validationCustom02" class="form-label">Apellido</label>
-        <input type="search" class="form-control" id="validationCustom02" name="apellidos"  required><br>
-      </div>
-      <div class="col-md-4">
-        <label for="validationCustomUsername" class="form-label">Registro</label>
-        <div class="input-group has-validation">
-          <input type="search" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" name="registro" required><br>
-        </div>
+        <input type="text" class="form-control" id="validationCustom02" name="apellido" placeholder="Apellido" required><br>
       </div>
 
       <div class="col-md-4">
-        <label for="validationCustomUsername2" class="form-label">Residencia</label>
-        <div class="input-group has-validation">
-          <input type="search" class="form-control" id="validationCustomUsername2" aria-describedby="inputGroupPrepend" name="residencia" required><br>
-        </div>
-
-        <label for="validationCustomUsername05" class="form-label">Municipio</label>
-        <div class="input-group has-validation">
-          <input type="search" class="form-control" id="validationCustomUsername05" aria-describedby="inputGroupPrepend" name="municipio" required><br>
-        </div>
+        <label for="validationCustom03" class="form-label">Registro</label>
+        <input type="text" class="form-control" id="validationCustom03" name="registro" placeholder="Registro" required><br>
       </div>
 
-
-      <div class="col-md-3">
-        <label for="validationCustom04" class="form-label">Busqueda por estado</label>
-        <select class="form-select" id="validationCustom04" name="estado"  required>
-          <option selected disabled value="">Estado...</option>
-          <option value="AGUACALIENTES">AGUACALIENTES</option>
-          <option value="BAJA CALIFORNIA">BAJA CALIFORNIA</option>
-          <option value="BAJA CALIFORNIA SUR">BAJA CALIFORNIA SUR</option>
-          <option value="CAMPECHE">CAMPECHE</option>
-          <option value="CHIAPAS">CHIAPAS</option>
-          <option value="CHIHUAHUA">CHIHAHUA</option>
-          <option value="CIUDAD DE MEXICO">CIUDAD DE MEXICO</option>
-          <option value="COAHUILA">COAHUILA</option>
-          <option value="COLIMA">COLIMA</option>
-          <option value="DURANGO">DURANGO</option>
-          <option value="ESTADO DE MEXICO">ESTADO DE MEXICO</option>
-          <option value="GUANAJUATO">GUANAJUATO</option>
-          <option value="GUERRERO">GUERRERO</option>
-          <option value="HIDALGO">HIDALGO</option>
-          <option value="JALISCO">JALISCO</option>
-          <option value="MICHOACAN">MICHOACAN</option>
-          <option value="MORELOS">MORELOS</option>
-          <option value="NARAYIT">NAYARIT</option>
-          <option value="NUEVO LEON">NUEVO LEON</option>
-          <option value="OAXACA">OAXACA</option>
-          <option value="PUEBLA">PUEBLA</option>
-          <option value="QUERETARO">QUERETARO</option>
-          <option value="QUINTANA ROO">QUINTANA ROO</option>
-          <option value="SAN LUIS POTOSI">SAN LUIS POTOSI</option>
-          <option value="SINALOA">SINALOA</option>
-          <option value="SONORA">SONORA</option>
-          <option value="TABASCO">TABASCO</option>
-          <option value="TAMAULIPAS">TAMAULIPAS</option>
-          <option value="TLAXCALA">TLAXCALA</option>
-          <option value="VERACRUZ">VERACRUZ</option>
-          <option value="YUCATAN">YUCATAN</option>
-          <option value="ZACATECAS">ZACATECAS</option>
-        </select><br>
-        <div class="invalid-feedback">
-          Selecciona un estado valido.
+      <div class="input-field col s12 m12 l6">
+            <select id="estado" name="estado" class="form-select" aria-label="Default select example"></select>
+            <label for="estado">Estado</label>
         </div>
-      </div>
+        <div class="input-field col s12 m12 l6">
+            <select id="municipio" name="municipio" class="form-select" aria-label="Default select example"></select>
+            <label for="municipio">Municipio</label>
+        </div>      
         <center>
 
           <button type="submit" name="buscar" class="btn btn-primary"><i class="fa fa-search"></i> buscar</button>
-          
         </center>
     </form>
-    <br>
     <a href="Agregar_Perito.php" class="btn btn-success "> + Agregar perito</a>
-    
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="municipios.js"></script>
+    <script type="text/javascript" src="select_estados.js"></script>
+   
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+          $('select').material_select();
+        });
+    </script>
+    <br>
+
   </div>
    <div id="tabla_peritos">
         <table class="table table-striped">
 
                                         
-                        <thead class="table-dark">    
+                        <thead>    
                         <tr>
                         <th>Nombre</th>
                         <th>Apellidos</th>
@@ -150,23 +115,39 @@
                                 $query = "SELECT * FROM contactos ORDER BY ID";
 
                                 if(isset($_POST['buscar'])){
-                                    $nombre=$conexion->real_escape_string($_POST['nombre']);
-                                    $apellido=$conexion->real_escape_string($_POST['apellidos']);
-                                    $registro=$conexion->real_escape_string($_POST['registro']);
-                                    $estado=$conexion->real_escape_string($_POST['estado']);
-                                    $residencia=$conexion->real_escape_string($_POST['residencia']);
-                                    $municipio=$conexion->real_escape_string($_POST['municipio']);
-                                    
-                                    $query = "SELECT * FROM contactos WHERE Nombre = '$nombre' OR Apellidos = '$apellido' OR Estado_provincia='$estado' OR Registros='$registro' OR Residencia='$residencia' OR Ciudad = '$municipio'";
+                                  $nombre = $conexion->real_escape_string($_POST['nombre']);
+                                  $apellido = $conexion->real_escape_string($_POST['apellido']); // Added line for last name
+                                  $estado = $conexion->real_escape_string($_POST['estado']); // Added line for state
+                                  $registro = $conexion->real_escape_string($_POST['registro']); 
+                                  $municipio = $conexion->real_escape_string($_POST['municipio']);
+                                  $query = "SELECT * FROM contactos WHERE Nombre LIKE '%$nombre%'";
+                              
+                                  // Check if the last name is provided, then include it in the query
+                                  if (!empty($apellido)) {
+                                      $query .= " AND Apellidos LIKE '%$apellido%'";
+                                  }
+                              
+                                  // Check if the state is selected, then include it in the query
+                                  if (!empty($estado)) {
+                                      $query .= " AND Estado_provincia = '$estado'";
+                                  }    
 
-                                }
-                                
+                                  if (!empty($registro)) {
+                                    $query .= " AND Registros LIKE '%$registro%'";
+                                  }
+
+                                  if (!empty($municipio)) {
+                                    $query .= " AND Ciudad = '$municipio'";
+                                  } 
+                              }
+            
                                 $resultado = $conexion->query($query);
                                 if($resultado -> num_rows > 0){
                                       while($fila = $resultado->fetch_assoc()){
                                   ?>  
                                   <tr>
-                                        <td><?php echo $fila['Nombre']; ?></td>
+          
+                                  <td><?php echo $fila['Nombre']; ?></td>
                                         <td><?php echo $fila['Apellidos']; ?></td>
                                         <td><?php echo $fila['Correoelectronico']; ?></td>
                                         <td><?php echo $fila['Telefonomovil']; ?></td>
@@ -175,11 +156,14 @@
                                         <td><?php echo $fila['Ciudad']; ?></td>
                                         <td><?php echo $fila['Estado_provincia']; ?></td>
                                         <td><?php echo $fila['Notas']; ?></td> 
-                                        <td><a class="btn btn-info" href="info.php?nombre=<?php echo $fila['Nombre']?>">info</a></td>
-                                        <td><i class="bi bi-exclamation-lg"><a class="btn btn-warning"  href="Editar_perito.php?nombre=<?php echo $fila['Nombre']?>">editar</a></i></td>
+                                        <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" name="mostrar" data-bs-target="#info<?php echo $fila['ID']?>">info</button></td>
+                                        <td><a class="btn btn-warning"  href="Editar_perito.php?nombre=<?php echo $fila['Nombre']?>">editar</a></td>
                                         <td><a class="btn btn-danger"  href="Eliminar_perito.php?nombre=<?php echo $fila['Nombre']?>">Eliminar</a></td>
+                                       
                                                 
                                   <?php 
+                                      include('info.php');
+                                      
                                       
                                                                       
                                       }
@@ -198,9 +182,7 @@
             </tbody> 
           </table>  
     
-
-    
    </div>
   
   </body>
-</html> 
+</html>
